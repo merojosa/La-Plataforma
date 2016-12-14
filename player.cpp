f322d651838e83664860200678b044c0d1072365
davidjj <alejandro.jimenezjimenez@ucr.ac.cr>
2016-12-14 16:10:02 -0600

    Si se presiona la tecla de abajo, el rectángulo reduce su tamaño a la mitad (se agacha), estando agachado, para levantarse se debe mover hacia uno de los lados.

--- player.cpp
+++ player.cpp
@@ -1,14 +1,42 @@
 #include "player.h"
+#include <QDebug>
 
 void Player::keyPressEvent(QKeyEvent *event)
 {
-    if(event->key() == Qt::Key_Left)
+    if(event->key() == Qt::Key_Left) //Izquierda, si esta agachado se levanta.
     {
+        if(rect().height() == 30)
+        {
+            setRect(0,0,30,60); //Cambia el tamaño.
+            setY(420); //Coordenada de la plataforma en y.
+        }
+
         setPos(x()-10, y());
     }
-    else if(event->key() == Qt::Key_Right)
+    else if(event->key() == Qt::Key_Right) //Derecha, si esta agachado se levanta
     {
+        if(rect().height() == 30)
+        {
+            setRect(0,0,30,60);
+            setY(420);
+        }
+
         setPos(x()+10, y());
+    }
+    else if(event->key() == Qt::Key_Down) //Si está levantado se agacha.
+    {
+        if(rect().height() != 30)
+        {
+            if(y() == 0)
+                setRect(0,y()+450,30,30);
+            else
+                setRect(0,30,30,30);
+        }
+
+    }
+    else if(event->key() == Qt::Key_Up) //Brinca
+    {
+
     }
 
 }
--- player.h
+++ player.h
@@ -8,6 +8,7 @@ class Player : public QGraphicsRectItem
 {
 public:
     void keyPressEvent(QKeyEvent* event);
+    //void keyReleaseEvent(QKeyEvent* event);
 };
 
 #endif // PLAYER_H
